56a6793f4b2bb33129334e2c15307e7ed8fa9f14
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-05 00:48:41 +0200

    set theme toggle size and icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import useDarkMode from '../../hooks/useDarkMode';
 import Toggle from '../Toggle';
+import MoonIcon from '../MoonIcon';
+import SunIcon from '../SunIcon';
 import styles from './index.module.css';
 
 const Header = () => {
   const [isDarkMode, toggleDarkMode] = useDarkMode();
+  const toggleSize = 50;
 
   return (
     <header className={styles.header}>
@@ -14,6 +17,9 @@ const Header = () => {
         isChecked={isDarkMode}
         onChange={toggleDarkMode}
         label={`Enable ${isDarkMode ? 'light' : 'dark'} mode`}
+        size={toggleSize}
+        uncheckedIcon={<MoonIcon size={toggleSize / 3} />}
+        checkedIcon={<SunIcon size={toggleSize / 3} />}
       />
     </header>
   );
