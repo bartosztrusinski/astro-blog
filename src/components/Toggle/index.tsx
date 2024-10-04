fa64bffa5d38cc03b4834cb246603cbe19b71b6f
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-05 00:19:55 +0200

    add size prop and wrap toggle with label

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,26 +1,32 @@
+import type { CSSProperties } from 'react';
 import styles from './index.module.css';
 
 interface ToggleProps {
   isChecked?: boolean;
   label: string;
+  size?: number;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Toggle = ({
   isChecked = false,
-  onChange: handleChange,
   label,
+  size = 48,
+  onChange: handleChange,
 }: ToggleProps) => {
   return (
-    <>
-      <label className={styles.visuallyHidden}>{label}</label>
+    <label
+      className={styles.toggleWrapper}
+      style={{ '--size': `${size / 16}rem` } as CSSProperties}
+    >
+      <span className={styles.visuallyHidden}>{label}</span>
       <input
         className={styles.toggle}
         type="checkbox"
         checked={isChecked}
         onChange={handleChange}
       />
-    </>
+    </label>
   );
 };
 
